2306d3ec2fc85b6ddd04cc3c2b860e3b3c623ef9
Sabbir Ahamed <54003568+SabbirAhamed007@users.noreply.github.com>
2020-08-16 11:55:50 -0400

    initial commit

---
+++ timefinds.py
@@ -0,0 +1,96 @@
+'''
+import time
+
+initial = time.time()
+print(initial)
+s = 0
+while(s<45):
+    print("This is Sabbir")
+    s+=1
+
+for i in range(45):
+    print("This is Sabbir")
+'''
+"""
+
+import time
+
+initial = time.time()
+
+s = 0
+while(s<45):
+    print("This is Sabbir")
+    s+=1
+print("While loop ran in ",time.time() - initial, "Seconds")
+
+
+initial2 = time.time()
+for i in range(45):
+    print("This is Sabbir")
+print("For loop ran in ",time.time() - initial2, "Seconds")
+
+"""
+
+"""
+import time
+
+initial = time.time()
+
+s = 0
+while(s<45):
+    print("This is Sabbir")
+    s+=1
+print("While loop ran in ",time.time() - initial, "Seconds")
+
+
+initial2 = time.time()
+for i in range(45):
+    print("This is Sabbir")
+print("For loop ran in ",time.time() - initial2, "Seconds")
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
+"""
+
+
+import time
+
+initial = time.time()
+
+s = 0
+while(s<45):
+    print("This is Sabbir")
+    time.sleep(1)
+    s+=1
+print("While loop ran in ",time.time() - initial, "Seconds")
+
+
+initial2 = time.time()
+for i in range(45):
+    print("This is Sabbir")
+print("For loop ran in ",time.time() - initial2, "Seconds")
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
+
+
+import time
+
+initial = time.time()
+
+s = 0
+while(s<45):
+    print("This is Sabbir")
+    s+=1
+print("While loop ran in ",time.time() - initial, "Seconds")
+
+
+initial2 = time.time()
+for i in range(45):
+    print("This is Sabbir")
+print("For loop ran in ",time.time() - initial2, "Seconds")
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
+
+
